41dd009afecdb534a31a90d302bc4b2087949733
John Heaven <john@johnheaven.eu>
2022-11-25 10:57:55 +0100

    now uses decorators; also added route for update page (not finished)

--- routes.py
+++ routes.py
@@ -1,15 +1,18 @@
-def wrap_route(route_function, *args, **kwargs):
-    # wrap a route similar to a functools partial
-    def wrapped_route_function(cl):
-        route_function(cl, *args, **kwargs)
+def wrap_route(route_function):
+    def wrapped_route_function(*args, **kwargs):
+        route_function(*args, **kwargs)
     return wrapped_route_function
 
-def identify_myself(cl, *args, **kwargs):
+@wrap_route
+def identify_myself(*args, **kwargs):
     from helpers.helpers import device_uuid_string
     unique_id = device_uuid_string()
+    
+    cl = args[0]
     cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
     cl.send(f"<html><body<><h1>{kwargs['pico_id']}</h1><p>It's me!</p><p>Unique ID: {unique_id}</p></body></html>")
 
+@wrap_route
 def ambient_data_readings(cl, **kwargs):
     import json
     # kwargs['ambient_data'] = the ambient_data generator
@@ -21,7 +24,17 @@ def ambient_data_readings(cl, **kwargs):
     cl.send('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n')
     cl.send(json.dumps(return_data))
 
+@wrap_route
 def not_found(cl, *args, **kwargs):
     # 404
     cl.send('HTTP/1.0 404 Not Found\r\nContent-type: text/html\r\n\r\n')
     cl.send(f'<html><body><h1>{kwargs['pico_id']}</h1><p>404: Resource not found.</p></body></html>')
+
+@wrap_route
+def update_settings(cl, *args, **kwargs):
+    # update settings e.g. wifi
+    template_list = []
+    replacements = {'pico_name': 'test name', 'ssid_name': 'blah blah', 'wifi_pass': 'testpass', 'bme280_checked': 'checked', 'dht22_checked': ''}
+
+    with open('templates/update.html', mode='r') as template:
+        cl.send("\n".join(list(template)).format(**replacements))
