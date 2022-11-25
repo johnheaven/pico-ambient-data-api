f5340e7f19440c9becaf7ee883f6d817970b1225
John Heaven <john@johnheaven.eu>
2022-11-25 10:57:29 +0100

    changed various things including support for decorators instaed of wrap function

--- main.py
+++ main.py
@@ -7,21 +7,49 @@ from routes import *
 # get the UUID of this machine
 my_uuid = device_uuid_string()
 # give this Pico an id so it can be found over the network
+
+if not my_uuid in global_settings.keys(): my_uuid = 'default'
+
 pico_id = global_settings[my_uuid]['pico_id']
 # determine the sensor type to use
 sensor_type = global_settings[my_uuid]['sensor_type']
 # secrets (wifi password etc.)
 secrets = global_settings[my_uuid]['secrets']
 
+
 # get a generator to yield readings one at a time
 ambient_data = get_ambient_data(iterations=True, sensor_type=sensor_type)
 
-ms = mini_server(secrets=secrets, rp2=rp2, not_found_response=wrap_route(not_found, pico_id=pico_id))
+ms = mini_server(
+    secrets=secrets,
+    rp2=rp2,
+    not_found_response=(not_found, {'pico_id': pico_id})
+    )
+    
 ms.connect_to_wifi()
 
 # add routes and handlers
-ms.add_route(('data',), wrap_route(ambient_data_readings, ambient_data=ambient_data, pico_id=pico_id, sensor_type=sensor_type, pico_uuid=my_uuid))
-ms.add_route(('find',), wrap_route(identify_myself, pico_id=pico_id))
+ms.add_route(
+    route=('data',),
+    handler=ambient_data_readings,
+    params={
+        'ambient_data': ambient_data,
+        'pico_id': pico_id,
+        'sensor_type': sensor_type,
+        'pico_uuid': my_uuid}
+    )
+
+ms.add_route(
+    route=('find',),
+    handler=identify_myself,
+    params={'pico_id': pico_id}
+    )
+
+ms.add_route(
+    route=('update',),
+    handler=update_settings,
+    params={}
+)
 
 # start listening
 ms.listen()
